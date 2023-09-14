7d7451542160a1dfd34ede9a51dcf82f0d3b6196
MJoonu <wnsdn8009@gmail.com>
2023-09-14 10:50:07 +0900

    Initial commit

---
+++ src/test/java/mini/item/ItemApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
